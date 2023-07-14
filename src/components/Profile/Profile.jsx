816f5abee63d709d46470fa7ab17e8e31a40cce5
Maryna-Lasitsyna <121902242+Maryna-Lasitsyna@users.noreply.github.com>
2023-07-14 14:24:48 +0300

    Beginning of work

---
+++ src/components/Profile/Profile.jsx
@@ -0,0 +1,55 @@
+import PropTypes from 'prop-types';
+import {
+  ProfileDiv,
+  Description,
+  Avatar,
+  Name,
+  Tag,
+  Location,
+  Stats,
+  StatsList,
+  SpanLabel,
+  SpanQuantity,
+} from './Profile.styled';
+
+const Profile = ({ username, tag, location, avatar, stats }) => {
+  return (
+    <ProfileDiv>
+      <Description>
+        <Avatar src={avatar} alt={username} />
+        <Name>{username}</Name>
+        <Tag>{tag}</Tag>
+        <Location>{location}</Location>
+      </Description>
+
+      <Stats>
+        <StatsList>
+          <SpanLabel>Followers</SpanLabel>
+          <SpanQuantity>{stats.followers}</SpanQuantity>
+        </StatsList>
+        <StatsList>
+          <SpanLabel>Views</SpanLabel>
+          <SpanQuantity>{stats.views}</SpanQuantity>
+        </StatsList>
+        <StatsList>
+          <SpanLabel>Likes</SpanLabel>
+          <SpanQuantity>{stats.likes}</SpanQuantity>
+        </StatsList>
+      </Stats>
+    </ProfileDiv>
+  );
+};
+
+Profile.propTypes = {
+  username: PropTypes.string.isRequired,
+  tag: PropTypes.string.isRequired,
+  location: PropTypes.string.isRequired,
+  avatar: PropTypes.string.isRequired,
+  stats: PropTypes.shape({
+    followers: PropTypes.number.isRequired,
+    views: PropTypes.number.isRequired,
+    likes: PropTypes.number.isRequired,
+  }).isRequired,
+};
+
+export default Profile;
---
+++ src/components/Profile/Profile.styled.jsx
@@ -0,0 +1,10 @@
+// import styled from 'clsx';
+
+// export const ProfileDiv = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   align-items: center;
+//   background-color: #fff;
+//   padding-top: 60px;
+//   padding-bottom: 60px;
+// `;
